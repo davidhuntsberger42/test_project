d206ceaeda4e561f3bcc6f617e9ab025fe3caab1
David Huntsberger <davidhuntsberger42@gmail.com>
2024-04-30 11:01:36 -0700

    finished todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ItemComponent],
+  template: '<h1>Todo list</h1>',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'test_project';
+  componentTitle = "To Do List";
+
+  //filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  get items() {
+    if (this.filter === "all" ) {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : item.done
+    );
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+
+  filter: "all" | "active" | "done" = "all";
 }
